6a40746d9cf90dc590fd7c4edab1119fbf166d4e
Eman Nazir <eman406261@gmail.com>
2026-07-25 14:09:26 +0500

    chore: update Resume model

--- server/src/models/Resume.js
+++ server/src/models/Resume.js
@@ -20,6 +20,6 @@ const resumeSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-resumeSchema.index({ userId: 1, createdAt: -1 }); // fast "latest versions" queries
+resumeSchema.index({ userId: 1, createdAt: -1 }); 
 
 export default mongoose.model("Resume", resumeSchema);
