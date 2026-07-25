bad8222e772f52b276c304f00fa4499305ba821a
Eman Nazir <eman406261@gmail.com>
2026-07-25 19:58:47 +0500

    chore: add root health check route and trust proxy setting

--- server/src/app.js
+++ server/src/app.js
@@ -15,6 +15,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const app = express();
 
 app.set('etag', false);
+app.set('trust proxy', 1);
 
 app.use(helmet());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -31,6 +32,10 @@ app.use(passport.initialize());
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-store');
   next();
+});
+
+app.get('/', (req, res) => {
+  res.json({ success: true, message: 'HireIQ API is running' });
 });
 
 app.use('/api/v1', apiLimiter);
