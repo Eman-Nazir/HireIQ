017e779e0c3a31faf04d3546b52ae5a01e985a46
Eman Nazir <eman406261@gmail.com>
2026-07-25 19:40:09 +0500

    fix: add MongoDB connection caching for serverless

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,16 @@
-import app from "../src/app.js"; 
+// api/index.js
+import mongoose from 'mongoose';
+import app from '../src/app.js';
 
-export default app;
+let isConnected = false;
+
+async function connectDB() {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  isConnected = true;
+}
+
+export default async function handler(req, res) {
+  await connectDB();
+  return app(req, res);
+}
