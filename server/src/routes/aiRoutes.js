9d2d7ffd8802801701a70009aab22d6247d933cf
Eman Nazir <eman406261@gmail.com>
2026-07-25 14:09:44 +0500

    chore: update AI routes

--- server/src/routes/aiRoutes.js
+++ server/src/routes/aiRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/authMiddleware.js';
+import { validate } from '../middleware/validateMiddleware.js';
+import { jobDescriptionSchema } from '../validators/aiValidators.js';
 import { scoreResume, getInterviewQuestions } from '../controllers/aiController.js';
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post('/score/:resumeId', scoreResume);
-router.post('/interview-prep/:resumeId', getInterviewQuestions);
+router.post('/score/:resumeId', validate(jobDescriptionSchema), scoreResume);
+router.post('/interview-prep/:resumeId', validate(jobDescriptionSchema), getInterviewQuestions);
 
 export default router;
